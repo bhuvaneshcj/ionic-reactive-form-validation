1295472a84b68659d5fe2a037ee840ab886d04bc
Bhuvanesh <bhuvanesh.c@synamen.com>
2023-03-17 15:36:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ionic Reactive Form Validation
+
+I'll demonstrate how to validate the reactive form in your ionic application
+
+## Getting started
+
+- [Download the installer](https://nodejs.org/) for Node LTS.
+- Install the Ionic CLI globally `npm install -g @ionic/cli`
+- Clone this repository `git clone https://github.com/bhuvaneshcj/ionic-reactive-form-validation.git`.
+- Run `npm install` from the project root.
+- Run `ionic serve` in a terminal from the project root.
+
+## Screenshot
+
+![DarkModeToggle](src/assets/screenshots/01.jpg)
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  form: any;
+  constructor() {}
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  // Initializing  form
+  initializeForm() {
+    this.form = new FormGroup({
+      name: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      email: new FormControl(null, {
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(8)],
+      }),
+    });
+  }
+
+  onSubmit() {
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    // Write your logic here ..
+  }
+}
